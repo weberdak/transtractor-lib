a296f8689a1de28f4034e3ddfc537bc93af308c8
Daniel <weberdk01@gmail.com>
2026-08-22 22:48:44 +0000

    chore: revert version back to beta

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("../python"))
 project = "Transtractor"
 copyright = "2026, Daniel Weber"
 author = "Daniel Weber"
-release = "1.0.0"
+release = "0.12.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
